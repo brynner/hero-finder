a2e0db8340f82d120f61213ab826fe575b30a646
Brynner Ferreira <ferreira@brynner.net>
2020-04-15 00:31:14 -0300

    Home

---
+++ app/src/components/sections/home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// UI
+import { Container } from '@material-ui/core';
+
+export default class Home extends Component {
+
+  render() {
+    return(
+      <main>
+        <Container maxWidth="md">
+          hello Jesus
+        </Container>
+      </main>
+    )
+  }
+
+}
