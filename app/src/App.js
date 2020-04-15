2a1f05bed27c57e3d5e561d090c839d26b983308
Brynner Ferreira <ferreira@brynner.net>
2020-04-15 00:32:08 -0300

    Acessando as rotas

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { routes } from "./utils/RouterUtil";
+
+// Sections
+import Home from './components/sections/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Route exact
+        path={routes.home.path}
+        render={(props) => <Home {...props} />}
+      />
+
     </div>
-  );
+  )
 }
 
 export default App;
