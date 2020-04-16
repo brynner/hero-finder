46e261dfe7a71ec7f25cb2941527d10339722e5b
Brynner Ferreira <ferreira@brynner.net>
2020-04-16 01:00:43 -0300

    Fetching some data with style

--- app/src/components/sections/home.jsx
+++ app/src/components/sections/home.jsx
@@ -1,18 +1,115 @@
 import React, { Component } from 'react';
+import CharactersController from '../../controllers/CharactersController';
 
-// UI
-import { Container } from '@material-ui/core';
+import styled from 'styled-components';
+import { Container, Card, CardMedia, CardContent, CardActionArea, Grid } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 export default class Home extends Component {
 
+  state = {
+    data: [],
+    results: []
+  };
+
+  componentDidMount() {
+    this.getCharacters();
+  }
+
+  getCharacters = () => {
+
+    CharactersController.getCharacters().then(result => {
+
+      console.log(result.data.data);
+
+      this.setState({
+        data: result.data.data.results
+      });
+
+    }).catch(result => {
+
+      console.log(result);
+
+    });
+  }
+
+  mountList = () => {
+    let items = [];
+    this.state.data.map((item, index) => {
+      return (
+        items.push(<div key={index}>
+
+          <Card className="card">
+
+            <CardActionArea>
+
+              {!item.thumbnail.path ? '' :
+                <CardMedia
+                  className="card-image"
+                  image={item.thumbnail.path + `.` + item.thumbnail.extension}
+                  title={item.name}
+                />
+              }
+
+              <CardContent>
+
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={6} md={3}>
+                    <Typography noWrap component="p">{item.name}</Typography>
+                  </Grid>
+                </Grid>
+
+              </CardContent>
+
+            </CardActionArea>
+
+          </Card>
+
+          <br /><br />
+        </div>)
+      )
+    })
+    return items;
+  }
+
   render() {
-    return(
-      <main>
-        <Container maxWidth="md">
-          hello Jesus
-        </Container>
-      </main>
+    return (
+      <Style>
+        <main className="component-main">
+          <Container maxWidth="md" className="component-list">
+
+            {
+              this.state.data && this.state.data.length ?
+                this.mountList()
+                :
+                'Loading...'
+            }
+
+          </Container>
+        </main>
+      </Style>
     )
   }
 
 }
+
+/**
+ * Style
+ */
+const Style = styled.div`
+  .component-main {
+
+    .component-list {
+      margin-top: 80px;
+    }
+
+    .card {
+      margin-bottom: 20px;
+
+      .card-image {
+        height: 100px;
+      }
+
+    }
+  }
+`
